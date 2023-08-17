9bac46626e254cd93017cf1ee48c0517408665ef
Hayden <me@haydenzeller.dev>
2023-08-17 18:26:02 -0400

    update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hayden Zeller.</h1>
+      <h2>Computer Science Student</h2>
+      <div className="contact">
+        <p>London, Ontario</p>
+        <a href="mailto:me@haydenzeller.dev">me@haydenzeller.dev</a>
+      </div>
+    </>
+  )
+}
+
+export default App
