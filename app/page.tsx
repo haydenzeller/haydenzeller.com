63a8fb178dd97c00b4412735df44786d3e1a4f9c
Hayden <hayden@haydenzeller.com>
2024-05-14 20:27:28 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">
-        Hayden Zeller
-      </h1>
+      <div className="rounded-lg p-5 bg-indigo-700">
+        <h1 className="text-5xl font-bold ">Hello World.</h1>
+        <p className="text-3xl">This is on the blockchain!</p>
+      </div>
     </main>
   );
 }
