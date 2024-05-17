5e63a0be16c7b85bb7cac24721f696b3f58cd349
Hayden <hayden@haydenzeller.com>
2024-05-17 18:34:48 -0400

    modal

---
+++ app/components/infoModal.tsx
@@ -0,0 +1,38 @@
+'use client'
+interface InfoModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+  }
+  
+  const InfoModal: React.FC<InfoModalProps> = ({ isOpen, onClose }) => {
+    const handleClose = () => {
+      onClose(); // Call the onClose function passed from the parent component to close the modal
+    };
+  
+    return (
+        <dialog id="infoModal" open={isOpen} onClose={onClose} className="modal  bg-gradient-to-r from-indigo-400 to-orange-400">
+            <div className="modal-box mx-auto bg-white">
+            <h3 className="font-bold text-black text-lg">Hello!</h3>
+            <p className="text-black font-bold text-lg">I put together this website to explore the capabilities of IPFS and .eth routing. It&apos;s built with NextJS, Tailwind, DaisyUI, and is hosted on <span className="bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text"><a href="https://fleek.xyz" target="_blank">fleek.xyz</a></span>. You can check out the source code on my <span className="bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text"><a href="https://github.com/haydenzeller/haydenzeller.com.git" target="_blank">GitHub</a></span>:)</p>
+            <br/>
+            <p className="font-bold text-black text-lg">Routes to this site:</p>
+            <ul>
+                <li><a className="font-bold text-lg bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text" href="https://haydenzeller.com">haydenzeller.com</a></li>
+                <li><a className="font-bold text-lg bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text" href="https://haydenzeller.eth.link">haydenzeller.eth.link</a></li>
+                <li><a className="font-bold text-lg bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text" href="https://haydenzeller.eth.limo">haydenzeller.eth.limo</a></li>
+                <li><p className="font-bold text-black text-lg">If your browser supports .eth try below:</p></li> 
+                <li><a className="font-bold text-lg bg-gradient-to-r from-indigo-500 via-green-500 to-violet-600 text-transparent bg-clip-text" href="https://haydenzeller.eth">haydenzeller.eth</a></li> 
+            </ul>
+            <div className="modal-action">
+                {/* Use onClick to handle the close button */}
+                <button className="btn" onClick={handleClose}>
+                    <p className="text-white font-bold text-xl">Close</p>
+                </button>
+            </div>
+            </div>
+        </dialog>
+    );
+  };
+  
+  export default InfoModal;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+'use client'
+import { useState } from "react";
 import githublogo from "../public/github-mark-white.png"
 import Link from "next/link";
 import x from "../public/x-logo.png"
 import ens from "../public/ens_mark_light.png"
 import Image from "next/image";
+import InfoModal from "./components/infoModal";
+
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  
   return (
-    <main className="flex min-h-screen flex-col items-center pt-2 pl-5 pr-5 bg-gradient-to-r from-indigo-400 to-orange-400">
+    <main className="flex min-h-screen max-w-screen flex-col items-center bg-gradient-to-r from-indigo-400 to-orange-400">
       <div className="rounded-lg p-5 mt-5 backdrop-contrast-200 backdrop-blur-3xl border-4">
         <h1 className="text-3xl font-bold ">Hi👋</h1>
         <p className="text-2xl font-bold">I&apos;m Hayden Zeller,</p>
@@ -16,24 +28,30 @@ export default function Home() {
         <div>
           <h1 className="text-3xl font-bold underline text-center">Find me here</h1>
           <div id="github" className="flex flex-row items-center p-2">
-            <Link href="https://github.com/haydenzeller" className="flex" target="_blank">
+            <Link href="https://github.com/haydenzeller" className="flex flex-row items-center" target="_blank">
               <Image src={githublogo} alt="githublogo" width={50}/>
               <p className="text-2xl p-2 hover:text-violet-500 cursor-pointer hover:underline">haydenzeller</p>
             </Link>
           </div>
           <div id="x" className="flex flex-row items-center p-2">
-            <Link href="https://x.com/haydendevs" className="flex" target="_blank">
+            <Link href="https://x.com/haydendevs" className="flex flex-row items-center" target="_blank">
               <Image src={x} alt="x logo (twitter)" width={50}/>
               <p className="text-2xl p-2 hover:text-violet-500 cursor-pointer hover:underline">@haydendevs</p>
             </Link>
           </div>
-          <div id="ens" className="p-2">
-            <Link href="https://app.ens.domains/haydenzeller.eth" className="flex" target="_blank">
+          <div id="ens" className="flex flex-row items-center p-2">
+            <Link href="https://app.ens.domains/haydenzeller.eth" className="flex flex-row items-center" target="_blank">
               <Image src={ens} alt="ens logo (twitter)" width={50}/>
               <p className="text-2xl p-2 hover:text-violet-500 cursor-pointer hover:underline">haydenzeller.eth</p>
             </Link>
           </div>
         </div>
+      </div>
+      <InfoModal isOpen={isModalOpen} onClose={closeModal}></InfoModal>
+      <div className="mt-4">
+        <button className="btn border-4 border-white bg-transparent backdrop-contrast-200" onClick={openModal}>
+          <p className="text-white font-bold text-lg">Site Info</p>
+        </button>
       </div>
     </main>
   );
