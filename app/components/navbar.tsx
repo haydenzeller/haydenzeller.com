b3b624bd81ef82d3036a6195b530dbc16e8cb794
Hayden <hayden@haydenzeller.com>
2024-05-21 13:44:38 -0400

    added triangles

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import getBtc from "../api/getBtc";
 import getExRate from "../api/getExRate";
 import btcLogo from "../../public/btcLogo.png"
+import redTriangle from "../../public/triangleRed.png"
+import greenTriangle from "../../public/triangleGreen.png"
 import Image from "next/image";
 export default function NavBar() {
   //Price Formatter
@@ -13,11 +15,13 @@ export default function NavBar() {
 
     //BTC Price
     const [btcPrice, setBTCPrice] = useState("");
+    const [isGreen, setIsGreen] = useState(() => Boolean(undefined))
     useEffect (() => {
         async function fetchData() {
         const price = await getBtc();
         const rate = await getExRate();
-        const format = formatter.format(price*rate);
+        const format = formatter.format(price.priceUsd*rate);
+        setIsGreen(price.changePercent24Hr > 0 ? true : false);
         setBTCPrice(format);
         }
         fetchData();
@@ -30,6 +34,7 @@ export default function NavBar() {
                 <a className="btn btn-ghost text-xl" target="_blank" href="https://www.coingecko.com/en/coins/bitcoin">
                     <Image src={btcLogo} alt="BTC Logo" width="25" />
                     {btcPrice !== "" ? (<h1>{btcPrice} CAD</h1>) : (<h1>Loading...</h1>)}
+                    {isGreen !== Boolean(undefined) ? isGreen ? (<Image src={greenTriangle} width="15" alt="green triangle"/>) : (<Image src={redTriangle} width="15" alt="red triangle"/>) : (<h1></h1>)}
                 </a>
             </div>
         </div>
